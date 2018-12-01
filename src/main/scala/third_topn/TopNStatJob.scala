c4152da2b27411814716fab964527f92b02dd8fb
LoneSurvivor1993 <tanzebin@foxmail.com>
2018-12-02 00:07:58 +0800

    获得Topn 视频课程，并且将数据添加到数据库

--- src/main/scala/third_topn/TopNStatJob.scala
+++ src/main/scala/third_topn/TopNStatJob.scala
@@ -1,8 +1,10 @@
 package third_topn
 
+import myutils.{DayVideoAccessStat, StatDAO}
 import org.apache.spark.sql.{DataFrame, SparkSession}
-
 import org.apache.spark.sql.functions._
+
+import scala.collection.mutable.ListBuffer
 
 /*
 * 对第二次清洗的结果进行统计
@@ -31,7 +33,32 @@ object TopNStatJob {
       " where day='20170511' and cmsType='video'" +
       " group by day,cmsId order by times desc")
 
-    videoAccessTopNDF.show(false)
+    //videoAccessTopNDF.show(false)
+
+    /*
+    * 将统计结果写入到数据库
+    * */
+    try {
+      videoAccessTopNDF.foreachPartition(partitionOfRecords => {
+        val list = new ListBuffer[DayVideoAccessStat]
+
+        partitionOfRecords.foreach(info => {
+          val day = info.getAs[String]("day")
+          val cmsId = info.getAs[Long]("cmsId")
+          val times = info.getAs[Long]("times")
+
+          list.append(DayVideoAccessStat(day, cmsId, times))
+
+        })
+
+        StatDAO.insertDayVideoAccessStat(list)
+
+      })
+    } catch {
+      case e: Exception => e.printStackTrace()
+    }
+
+
   }
 
   def main(args: Array[String]): Unit = {
