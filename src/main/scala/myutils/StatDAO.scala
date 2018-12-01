cdcd4a0243e026fbc2b82b44214f9a1dd375e841
LoneSurvivor1993 <tanzebin@foxmail.com>
2018-12-01 23:52:34 +0800

    各个维度统计的DAO操作

---
+++ src/main/scala/myutils/StatDAO.scala
@@ -0,0 +1,43 @@
+package myutils
+
+import java.sql.{Connection, PreparedStatement}
+
+import scala.collection.mutable.ListBuffer
+
+/*
+* 各个维度统计的DAO操作
+* */
+object StatDAO {
+  /*
+  * 批量保存 DayVideoAccessStat 到数据库
+  * */
+  def insertDayVideoAccessStat(list: ListBuffer[DayVideoAccessStat]) = {
+    var connection: Connection = null
+    var pstmt: PreparedStatement = null
+
+    connection.setAutoCommit(false)
+
+    try {
+      val sql = "insert into table day_video_access_topn_stat(day,cms_id,times) values (?,?,?)"
+      pstmt = connection.prepareStatement(sql)
+      for (ele <- list) {
+        /*
+        * 批处理使用手动提交
+        * */
+        pstmt.setString(1, ele.day)
+        pstmt.setLong(2, ele.cmsId)
+        pstmt.setLong(3, ele.times)
+
+        pstmt.addBatch()
+      }
+      pstmt.executeBatch() //执行批量处理
+      connection.commit() //手工提交
+
+
+    } catch {
+      case e: Exception => e.printStackTrace()
+    } finally {
+      MySQLUtils.release(connection, pstmt)
+    }
+  }
+}
