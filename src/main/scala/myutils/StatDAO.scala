305ed387bc597a617cbaf6734a16b511cae918be
LoneSurvivor1993 <tanzebin@foxmail.com>
2018-12-02 15:03:53 +0800

    删除指定日期的数据

--- src/main/scala/myutils/StatDAO.scala
+++ src/main/scala/myutils/StatDAO.scala
@@ -97,8 +97,8 @@ object StatDAO {
         pstmt.setString(1, ele.day)
         pstmt.setLong(2, ele.cmsId)
         pstmt.setString(3, ele.city)
-        pstmt.setLong(4,ele.times)
-        pstmt.setInt(5,ele.timesRank)
+        pstmt.setLong(4, ele.times)
+        pstmt.setInt(5, ele.timesRank)
 
         pstmt.addBatch()
       }
@@ -148,6 +148,37 @@ object StatDAO {
     }
   }
 
+  /*
+  * 删除指定日期的数据
+  * */
+  def deleteData(day: String) = {
+    val tables = Array(
+      "day_video_access_topn_stat",
+      "day_video_city_access_topn_stat",
+      "create table day_video_traffics_topn_stat"
+    )
+
+    var connection: Connection = null
+    var pstmt: PreparedStatement = null
+
+    try {
+      connection = MySQLUtils.getConnection()
+
+      for (table <- tables) {
+        val deleteSQL = s"delete from $table where day=?"
+        pstmt = connection.prepareStatement(deleteSQL)
+        pstmt.setString(1,day)
+        pstmt.executeUpdate()
+      }
+
+    } catch {
+      case e: Exception => e.printStackTrace()
+    } finally {
+      MySQLUtils.release(connection, pstmt)
+    }
+
+
+  }
 
 
 }
