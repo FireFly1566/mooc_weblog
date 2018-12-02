bd1f1b06b6e141b67f52ab41561323dfdf7b4cf6
LoneSurvivor1993 <tanzebin@foxmail.com>
2018-12-02 14:26:46 +0800

    去掉注释

--- src/main/scala/myutils/MySQLUtils.scala
+++ src/main/scala/myutils/MySQLUtils.scala
@@ -2,18 +2,6 @@ package myutils
 
 import java.sql.{Connection, DriverManager, PreparedStatement}
 
-
-/*
-* 数据库建表
-* create table day_video_access_topn_stat(
-* day varchar(8) not null,
-* cms_id bigint(10) not null,
-* times bigint(10) not null,
-* primary key (day,cms_id)
-* );
-*
-* */
-
 /*
 * MySQL 操作工具类
 * */
